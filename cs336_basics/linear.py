d1233a21d5812866db57312e0aa0107b53b8ca0f
steven14414 <steven144@foxmail.com>
2026-06-21 16:41:23 +0800

    3.3.2Linear Module

---
+++ cs336_basics/linear.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+from einops import rearrange, einsum
+
+
+class Linear(nn.Module):
+    def __init__(self, in_features, out_features, device=None, dtype=None):
+        factory_kwargs = {"device": device, "dtype": dtype}
+        super().__init__()
+        self.weight = nn.Parameter(torch.empty((out_features, in_features), **factory_kwargs))
+        std = (2 / (in_features + out_features)) ** 0.5
+        nn.init.trunc_normal_(self.weight, mean=0, std=std, a=-3 * std, b=3 * std)
+
+    def forward(self, x):
+        return einsum(x, self.weight, "... d_in,d_out d_in->... d_out")
--- tests/adapters.py
+++ tests/adapters.py
@@ -28,8 +28,11 @@ def run_linear(
     Returns:
         Float[Tensor, "... d_out"]: The transformed output of your linear module.
     """
+    from cs336_basics.linear import Linear
 
-    raise NotImplementedError
+    lin = Linear(d_in, d_out)
+    lin.load_state_dict({"weight": weights})
+    return lin(in_features)
 
 
 def run_embedding(
