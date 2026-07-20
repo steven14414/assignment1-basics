e9e9adc03899d9ea5d2643d53f40e8bf7a312775
steven14414 <steven144@foxmail.com>
2026-07-20 18:37:23 +0800

    4.1 Cross-entropy loss

---
+++ cs336_basics/cross_entropy.py
@@ -0,0 +1,8 @@
+import torch
+
+
+def cross_entropy(logits, x):
+    logits = logits - torch.amax(logits, dim=-1, keepdim=True)
+    log_sum_exp = torch.log(torch.sum(torch.exp(logits), dim=-1))
+    loss = log_sum_exp - logits[torch.arange(logits.size(0)), x]
+    return loss.mean()
