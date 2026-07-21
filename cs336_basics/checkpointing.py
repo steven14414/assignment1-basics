d7d7d1d0c17420bdfdb96223fb22e93f043764f0
steven14414 <steven144@foxmail.com>
2026-07-21 10:25:43 +0800

    5.2 Checkpointing

---
+++ cs336_basics/checkpointing.py
@@ -0,0 +1,24 @@
+import torch
+import os
+from typing import IO, Any, BinaryIO
+
+
+def save_checkpoint(
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+    iteration: int,
+    out: str | os.PathLike | BinaryIO | IO[bytes],
+):
+    obj = {"model": model.state_dict(), "optimizer": optimizer.state_dict(), "iteration": iteration}
+    torch.save(obj, f=out)
+
+
+def load_checkpoint(
+    src: str | os.PathLike | BinaryIO | IO[bytes],
+    model: torch.nn.Module,
+    optimizer: torch.optim.Optimizer,
+) -> int:
+    obj = torch.load(src)
+    model.load_state_dict(obj["model"])
+    optimizer.load_state_dict(obj["optimizer"])
+    return obj["iteration"]
